0d06697c2a9a7cd904736066cc5475dba7e94547
ahiru <wellingtonnmaeda0524@gmail.com>
2019-06-05 21:28:28 +0900

    prettierによる整形

--- src/models/response.ts
+++ src/models/response.ts
@@ -1,81 +1,87 @@
 function init<T>(a: T, b: T) {
-    Object.assign(a, b);
+  Object.assign(a, b);
 }
 
 export interface MessageData {
-    id: string;
-    type: string;
-    created_at: string;
-    visibility: string;
-    url: string;
-    content: string;
-    account: any;
-    status: any;
-    reblogs_count: number;
+  id: string;
+  type: string;
+  created_at: string;
+  visibility: string;
+  url: string;
+  content: string;
+  account: any;
+  status: any;
+  reblogs_count: number;
 }
 
 export interface ResponseData {
-    event?: string;
-    data: MessageData;
+  event?: string;
+  data: MessageData;
 }
 
-
-export interface Response extends ResponseData {
-}
+export interface Response extends ResponseData {}
 
 export class Response {
+  constructor(msg: any) {
+    init<ResponseData>(this, msg);
+  }
 
-    constructor(msg: any) {
-        init<ResponseData>(this, msg);
-    }
+  /**
+   * 反応するイベント
+   * 可燃性のメッセージに反応する
+   */
+  isInflammable(): boolean {
+    return (
+      this.event === 'update' ||
+      (this.event === 'notification' && this.data.type === 'mention')
+    );
+  }
 
-    /**
-     * 反応するイベント
-     * 可燃性のメッセージに反応する
-     */
-    isInflammable(): boolean {
-        return this.event === 'update' || this.event === 'notification' && this.data.type === 'mention';
+  id(): string {
+    if (this.event === 'notification' && this.data.type === 'mention') {
+      return this.data.status.id;
+    } else {
+      return this.data.id;
     }
+  }
 
-    id(): string {
-        if (this.event === 'notification' && this.data.type === 'mention') {
-            return this.data.status.id;
-        } else {
-            return this.data.id;
-        }
-    }
+  /**
+   * https://example.com/@sample => @sample@example.com
+   */
+  username(): string {
+    const url =
+      this.data.account.url ||
+      (this.data.status &&
+        this.data.status.account &&
+        this.data.status.account.url);
+    const [, hostname, username] =
+      url.match(/https:\/\/(.+)\/(@.+)/) || new Array(3);
+    return `${username}@${hostname}`;
+  }
 
-    /**
-     * https://example.com/@sample => @sample@example.com
-     */
-    username(): string {
-        const url = this.data.account.url || this.data.status && this.data.status.account && this.data.status.account.url;
-        const [, hostname, username] = url.match(/https:\/\/(.+)\/(@.+)/) || new Array(3);
-        return `${username}@${hostname}`;
+  visibility(): string {
+    if (this.event === 'notification' && this.data.type === 'mention') {
+      return this.data.status.visibility;
+    } else {
+      return this.data.visibility;
     }
+  }
 
-    visibility(): string {
-        if (this.event === 'notification' && this.data.type === 'mention') {
-            return this.data.status.visibility;
-        } else {
-            return this.data.visibility;
-        }
-    }
+  /**
+   * 本文
+   * `@ahiruyaki` に対するリプライ文字列に反応しないようにあらかじめ消しておく
+   */
+  content() {
+    const re = /@(<span>)?ahiruyaki/g;
+    const content =
+      this.data.content || (this.data.status && this.data.status.content) || '';
+    return content.replace(re, '$1');
+  }
 
-    /**
-     * 本文
-     * `@ahiruyaki` に対するリプライ文字列に反応しないようにあらかじめ消しておく
-     */
-    content() {
-        const re = /@(<span>)?ahiruyaki/g;
-        const content = this.data.content || (this.data.status && this.data.status.content) || '';
-        return content.replace(re, '$1');
-    }
-
-    /**
-     * 生成日時
-     */
-    createdAt() {
-        return new Date(this.data.created_at);
-    }
+  /**
+   * 生成日時
+   */
+  createdAt() {
+    return new Date(this.data.created_at);
+  }
 }
--- test/yakuna.test.ts
+++ test/yakuna.test.ts
@@ -5,130 +5,158 @@ import { Yakuna, あひる焼き, ReplaceCase } from '../src/yakuna';
 import { Dictionary } from '../src/models/dictionary';
 
 describe('env', () => {
-    const message = 'refer README.adoc and set right env';
+  const message = 'refer README.adoc and set right env';
 
-    afterEach('reset env', () => {
-        // https://nodejs.org/api/process.html#process_process_env
-        // process.env は string しか持たない
-        // そのため undefined を渡しても 'undefined' という文字列が格納されてしまう
-        // 削除したい場合は以下のように delete する必要がある
-        //  ok: delete process.env.FOO
-        //  ng: process.env.FOO = undefined
-        delete process.env.ACCESS_TOKEN;
-        delete process.env.API_URL;
-    });
+  afterEach('reset env', () => {
+    // https://nodejs.org/api/process.html#process_process_env
+    // process.env は string しか持たない
+    // そのため undefined を渡しても 'undefined' という文字列が格納されてしまう
+    // 削除したい場合は以下のように delete する必要がある
+    //  ok: delete process.env.FOO
+    //  ng: process.env.FOO = undefined
+    delete process.env.ACCESS_TOKEN;
+    delete process.env.API_URL;
+  });
 
-    it('should fail instantiate', () => {
-        assert.throws(
-            () => new Yakuna({ access_token: '', api_url: '' }),
-            (err: Error) => {
-                assert.equal(err.name, 'Error');
-                assert.equal(err.message, 'Mastodon config must include \'access_token\' when using \'user_auth\'');
-                return !!err;
-            },
-            message
+  it('should fail instantiate', () => {
+    assert.throws(
+      () => new Yakuna({ access_token: '', api_url: '' }),
+      (err: Error) => {
+        assert.equal(err.name, 'Error');
+        assert.equal(
+          err.message,
+          "Mastodon config must include 'access_token' when using 'user_auth'"
         );
-    });
+        return !!err;
+      },
+      message
+    );
+  });
 
-    it('should have access_token', function () {
-        config();
-        assert.notStrictEqual(process.env.ACCESS_TOKEN, undefined, message);
-        assert.notStrictEqual(process.env.ACCESS_TOKEN, '', message);
-    });
+  it('should have access_token', function() {
+    config();
+    assert.notStrictEqual(process.env.ACCESS_TOKEN, undefined, message);
+    assert.notStrictEqual(process.env.ACCESS_TOKEN, '', message);
+  });
 
-    it('should have api_url', () => {
-        config();
-        assert.notStrictEqual(process.env.API_URL, undefined, message);
-        assert.notStrictEqual(process.env.API_URL, '', message);
-    });
+  it('should have api_url', () => {
+    config();
+    assert.notStrictEqual(process.env.API_URL, undefined, message);
+    assert.notStrictEqual(process.env.API_URL, '', message);
+  });
 
-    it('should instantiate', function () {
-        config();
-        assert.doesNotThrow(
-            () => new Yakuna({
-                access_token: process.env.ACCESS_TOKEN as string,
-                api_url: process.env.API_URL as string
-            }),
-            message
-        );
-    });
+  it('should instantiate', function() {
+    config();
+    assert.doesNotThrow(
+      () =>
+        new Yakuna({
+          access_token: process.env.ACCESS_TOKEN as string,
+          api_url: process.env.API_URL as string
+        }),
+      message
+    );
+  });
 });
 
 describe('dictionary', () => {
-    it('should have right props`', () => {
-        const sampleData = ['sample', 'example'];
-        const dictionary = new Dictionary({ japanese: sampleData });
-        assert.deepEqual(dictionary.japanese, ['sample', 'example']);
-    });
+  it('should have right props`', () => {
+    const sampleData = ['sample', 'example'];
+    const dictionary = new Dictionary({ japanese: sampleData });
+    assert.deepEqual(dictionary.japanese, ['sample', 'example']);
+  });
 });
 
 describe('yakuna', () => {
-    let yakuna: Yakuna;
+  let yakuna: Yakuna;
 
-    beforeEach('set env', () => {
-        config();
-        yakuna = new Yakuna({
-            access_token: process.env.ACCESS_TOKEN as string,
-            api_url: process.env.API_URL as string
-        });
+  beforeEach('set env', () => {
+    config();
+    yakuna = new Yakuna({
+      access_token: process.env.ACCESS_TOKEN as string,
+      api_url: process.env.API_URL as string
     });
+  });
 
-    afterEach('reset env', () => {
-        delete process.env.ACCESS_TOKEN;
-        delete process.env.API_URL;
-    });
+  afterEach('reset env', () => {
+    delete process.env.ACCESS_TOKEN;
+    delete process.env.API_URL;
+  });
 
-    it('should have right regex', () => {
-        assert.equal(
-            String(あひる焼き),
-            String(/([あアｱ][ひヒﾋ][るルﾙ]|家鴨)[やヤﾔ焼][きキｷ]|ahiruyaki|扒家鸭|3v\.7g/i),
-            'unexpected regex'
-        );
-    });
+  it('should have right regex', () => {
+    assert.equal(
+      String(あひる焼き),
+      String(
+        /([あアｱ][ひヒﾋ][るルﾙ]|家鴨)[やヤﾔ焼][きキｷ]|ahiruyaki|扒家鸭|3v\.7g/i
+      ),
+      'unexpected regex'
+    );
+  });
 
-    it('should remove zero width space', () => {
-        // from asyley_
-        const testCase = 'あㅤひる焼き';
+  it('should remove zero width space', () => {
+    // from asyley_
+    const testCase = 'あㅤひる焼き';
 
-        const replaced = testCase.replace(ReplaceCase, '');
-        assert.equal(replaced, 'あひる焼き');
-    });
+    const replaced = testCase.replace(ReplaceCase, '');
+    assert.equal(replaced, 'あひる焼き');
+  });
 
-    it('should remove space', () => {
-        // from 2bo
-        const testCase = 'A H I R U Y A K I';
+  it('should remove space', () => {
+    // from 2bo
+    const testCase = 'A H I R U Y A K I';
 
-        const replaced = testCase.replace(ReplaceCase, '');
-        assert.equal(replaced, 'AHIRUYAKI');
-    });
+    const replaced = testCase.replace(ReplaceCase, '');
+    assert.equal(replaced, 'AHIRUYAKI');
+  });
 
-    it('should match regex', () => {
-        const regex = あひる焼き;
-        const testCase = [
-            'あひる焼き', '家鴨焼き', 'あひるやき', 'ahiruyaki', '扒家鸭', '3v.7g',
-            'あひル焼ｷ', 'アﾋるやキ', '家鴨やｷ', 'AhiRuYaki', 'あㅤひる焼き'
-        ];
+  it('should match regex', () => {
+    const regex = あひる焼き;
+    const testCase = [
+      'あひる焼き',
+      '家鴨焼き',
+      'あひるやき',
+      'ahiruyaki',
+      '扒家鸭',
+      '3v.7g',
+      'あひル焼ｷ',
+      'アﾋるやキ',
+      '家鴨やｷ',
+      'AhiRuYaki',
+      'あㅤひる焼き'
+    ];
 
-        testCase.forEach((text) => assert.ok(regex.test(text.replace(ReplaceCase, ''))));
-    });
+    testCase.forEach(text =>
+      assert.ok(regex.test(text.replace(ReplaceCase, '')))
+    );
+  });
 
-    it('should not match regex', () => {
-        const regex = あひる焼き;
-        const testCase = [
-            'ahiru焼き', '焼きあひる',
-        ];
+  it('should remove new line and match multiline yaki', () => {
+    const regex = あひる焼き;
+    const testCase = `日直
+    あ
+    ひ
+    る
+    焼
+    き`;
 
-        testCase.forEach((text) => assert.ok(!regex.test(text)));
-    });
+    const replaced = testCase.replace(ReplaceCase, '');
+    assert.ok(regex.test(replaced));
+  });
 
-    it('should load dictionary', () => {
-        const dictionary = (yakuna as any).loadDictionary();
+  it('should not match regex', () => {
+    const regex = あひる焼き;
+    const testCase = ['ahiru焼き', '焼きあひる'];
 
-        assert.equal(Object.keys(dictionary).length, 5);
-        assert.ok(dictionary['chinese'].length > 0);
-        assert.ok(dictionary['english'].length > 0);
-        assert.ok(dictionary['japanese'].length > 0);
-        assert.ok(dictionary['meshitero'].length > 0);
-        assert.ok(dictionary['shogatsu'].length > 0);
-    });
+    testCase.forEach(text => assert.ok(!regex.test(text)));
+  });
+
+  it('should load dictionary', () => {
+    const dictionary = (yakuna as any).loadDictionary();
+
+    assert.equal(Object.keys(dictionary).length, 5);
+    assert.ok(dictionary['chinese'].length > 0);
+    assert.ok(dictionary['english'].length > 0);
+    assert.ok(dictionary['japanese'].length > 0);
+    assert.ok(dictionary['meshitero'].length > 0);
+    assert.ok(dictionary['shogatsu'].length > 0);
+  });
 });
